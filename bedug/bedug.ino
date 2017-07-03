16adba499695bcdbd6c0bf086fa868df8c02e78f
Hisyamkamil <hisyamkamilchandrazaman@gmail.com>
2017-07-03 21:02:25 +0700

    Add files via upload

---
+++ bedug/bedug.ino
@@ -0,0 +1,117 @@
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+#define VS1053_RX  15
+#define VS1053_RESET 10
+//#define VS1053_BANK_DEFAULT 0x00
+#define VS1053_BANK_DRUMS1 0x78
+#define VS1053_BANK_DRUMS2 0x7F
+//#define VS1053_BANK_MELODY 0x79
+
+#define VS1053_GM1_OCARINA 36
+
+#define MIDI_NOTE_ON  0x90
+#define MIDI_NOTE_OFF 0x80
+#define MIDI_CHAN_MSG 0xB0
+#define MIDI_CHAN_BANK 0x00
+#define MIDI_CHAN_VOLUME 0x07
+#define MIDI_CHAN_PROGRAM 0xC0
+
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+
+SoftwareSerial VS1053_MIDI(0, 15);
+int pitch[]={80, 47, 48};
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("VS1053 MIDI test"); 
+  VS1053_MIDI.begin(31250); // MIDI uses a 'strange baud rate'
+
+    if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+
+  
+  pinMode(VS1053_RESET, OUTPUT);
+  digitalWrite(VS1053_RESET, LOW);
+  delay(10);
+  digitalWrite(VS1053_RESET, HIGH);
+  delay(10);
+
+  midiSetChannelBank(1, VS1053_BANK_DRUMS1);
+  midiSetInstrument(1, VS1053_GM1_OCARINA);
+  midiSetChannelVolume(1, 60);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+      currtouched = cap.touched(); 
+  for (uint8_t i=1; i<3; i++) {
+    // it if *is* touched and *wasnt* touched before, alert!
+    if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" touched");
+      midiNoteOn(1, pitch[i], 127);
+    }
+    // if it *was* touched and now *isnt*, alert!
+    if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+      Serial.print(i); Serial.println(" released");
+      midiNoteOff(1, pitch[i], 127);
+    }
+  }
+   lasttouched = currtouched;      
+  delay(50);
+}
+
+void midiSetInstrument(uint8_t chan, uint8_t inst) {
+  if (chan > 15) return;
+  inst --; // page 32 has instruments starting with 1 not 0 :(
+  if (inst > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_PROGRAM | chan);  
+  VS1053_MIDI.write(inst);
+}
+
+
+void midiSetChannelVolume(uint8_t chan, uint8_t vol) {
+  if (chan > 15) return;
+  if (vol > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write(MIDI_CHAN_VOLUME);
+  VS1053_MIDI.write(vol);
+}
+
+void midiSetChannelBank(uint8_t chan, uint8_t bank) {
+  if (chan > 15) return;
+  if (bank > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write((uint8_t)MIDI_CHAN_BANK);
+  VS1053_MIDI.write(bank);
+}
+
+void midiNoteOn(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+  
+  VS1053_MIDI.write(MIDI_NOTE_ON | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
+
+void midiNoteOff(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+  
+  VS1053_MIDI.write(MIDI_NOTE_OFF | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
